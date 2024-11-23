c078b9db157ce60818fab71b51a044d83cb08be2
DATDQ1510 <datdq5275@gmail.com>
2024-11-23 17:04:43 +0700

    create and run server

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon, FaSun } from 'react-icons/fa';
 
-export default function header() {
+export default function Header() {
   return (
-    <div>
-      hello
-    </div>
-  )
+    <Navbar className='border-b-2'>
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Dats's
+        </span>
+        Blog
+      </Link>
+      <form >
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className='flex gap-2 md:order-2'>
+        <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill>
+          <FaMoon />
+        </Button>
+
+        <Link to='/sign-in'>
+          <Button gradientDouTone='purleToBlue' outline>
+            Sign In
+          </Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link >
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link >
+          <Link to='/about'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link >
+          <Link to='/projects'>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
